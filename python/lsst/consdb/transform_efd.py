77100ba7e5681a6e0e4b8bf44f255e2e8d535a25
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2024-05-07 17:43:01 -0700

    An initial structure was implemented that allows iterating through the output columns, calculating the average recovered from the topics.

--- python/lsst/consdb/transform_efd.py
+++ python/lsst/consdb/transform_efd.py
@@ -22,7 +22,19 @@ def gen_mean(
     def do(
         series: pandas.DataFrame, start: astropy.time.Time, end: astropy.time.Time
     ) -> Summary:
-        return Summary()
+        
+        df = series.loc[(series.index > str(start)) & (series.index <= str(end))]
+
+        # print(df)
+        # print(df.info(verbose=True))
+
+        # TODO aqui aplicaria a funcao de media no dataframe
+        # TODO: Como aplicar a função quando houver mais de uma coluna?
+        result = df[df.columns[0]].mean()
+        print(f"Sumary: {start} -> {end} Column: {df.columns[0]} Count: {len(df)} Result: {result}")
+
+        return result
+        # return Summary()
 
     return do
 
@@ -34,24 +46,27 @@ class EfdValues:
     def __init__(
         self,
         config: dict[str, Any],
-        window: astropy.time.TimeDelta,
         series: pandas.DataFrame,
+        window: astropy.time.TimeDelta = 0.0,        
     ):
         self._entries = series
         self._sum_function = FUNCTION_GENERATORS[config["function"]](config)
-        self._window = window
+        self._window = astropy.time.TimeDelta(window, format="sec")
 
     def summarize(self, start: astropy.time.Time, end: astropy.time.Time) -> Any:
         return self._sum_function(
-            self._entries, start - self._window, end + self._window
+            self._entries, start.utc - self._window, end.utc + self._window
         )
 
 
 class Records:
     def __init__(self, db: Engine):
         self._db = db
+        self.rows = []
 
     def add(self, dim: DimensionRecord, topic: dict[str, Any], summary: Any) -> None:
+
+        new_row = dim.toDict()
         pass
 
     def write(self, table: str) -> None:
@@ -77,13 +92,23 @@ async def get_efd_values(
         end.utc + window,
         topic.get("index", None),
     )
-    series.to_csv(".tmp/" + topic["name"] + ".csv")
+    # series.to_csv(".tmp/" + topic["name"] + ".csv")
     print(f"Topic: {topic['name']}")
-    # print(f"Window: {window}")
-    # print(f"Series: {len(series)}")
-    # print(series)
+    print(f"Rows: {len(series)}")
+    # print(f"Series: {type(series)}")
+    # print(series.dtypes)
 
-    return EfdValues(topic, window, series)
+
+    # TODO: Fazendo um resample e interpolate Provisório.
+    # Somente para simular que existe mais de uma mensagem por periodo da exposição
+    # e permitir que seja feito a sumarização.
+    series = series.resample('1s', origin=series.index[0]).mean()
+    series = series.interpolate(method="time")
+
+    print(f"Rows After resample: {len(series)}")
+
+    return series
+    # return EfdValues(topic, window, series)
 
 
 def get_exposures_by_period(
@@ -97,9 +122,14 @@ def get_exposures_by_period(
     where_clause = (
         f"instrument=instr and exposure.timespan OVERLAPS (T'{start}', T'{end}')"
     )
+    if limit is not None:
+        return butler.registry.queryDimensionRecords(
+            "exposure", where=where_clause, bind=dict(instr=instrument)
+        ).limit(limit)
+    
     return butler.registry.queryDimensionRecords(
         "exposure", where=where_clause, bind=dict(instr=instrument)
-    ).limit(limit)
+    )
 
 
 def get_visits_by_period(
@@ -113,13 +143,57 @@ def get_visits_by_period(
     where_clause = (
         f"instrument=instr and visit.timespan OVERLAPS (T'{start}', T'{end}')"
     )
-    return butler.registry.queryDimensionRecords(
-        "visit", where=where_clause, bind=dict(instr=instrument)
-    ).limit(limit)
+
+    if limit is not None:
+        return butler.registry.queryDimensionRecords(
+            "visit", where=where_clause, bind=dict(instr=instrument)
+        ).limit(limit)
+    
+    butler.registry.queryDimensionRecords(
+            "visit", where=where_clause, bind=dict(instr=instrument)
+        )
 
 
 def butler_query_results_to_pandas(query):
     return pandas.DataFrame([q.toDict() for q in query])
+
+def proccess_column(column, data, begin, end):
+
+    # print(column)
+    df = None
+    for topic in column["topics"]:
+        topic_name = topic["topic"]
+        topic_fields = topic["fields"]
+
+        topic_df = data[topic_name]
+        # print(topic_df)
+        # subset_df = topic_df[topic_fields]        
+        # print(subset_df)        
+        if df is None:
+            # df = topic_df[topic_df.columns[0]]
+            df = topic_df[topic_fields].copy()
+            # df.rename(columns={df.columns[0]: 'index'}, inplace=True)
+            # df[df.columns[0]] = pandas.to_datetime(df[df.columns[0]])
+            # df = df.set_index(df.columns[0])
+        else:
+            # TODO: Aqui é necessário uma solução para concatenar os subset_df baseado no index date_time. 
+            # Por que cada topico pode ter quantidades de linhas diferentes. 
+            pass
+
+    # print(df)
+    # print(df.info(verbose=True))
+
+    # Neste momento já tem o dataframe com todos os campos/topicos necessários para processar a colunas.
+    efd_values = EfdValues(config={"function": column["function"]}, series=df, window=column["window"])
+
+    # TODO: TEste usando valores maiores que o periodo da exposicao
+    # begin = astropy.time.Time("2024-04-25T00:00:00", format="isot", scale="utc")
+    # end = astropy.time.Time("2024-04-25T23:59:59", format="isot", scale="utc")
+
+    summary = efd_values.summarize(begin, end)
+    return summary
+
+
 
 
 async def process_interval(
@@ -145,23 +219,50 @@ async def process_interval(
     min_topic_time = end
     max_topic_time = start
 
-    for record in get_exposures_by_period(butler, instrument, start, end, limit=10):
+    # for record in get_exposures_by_period(butler, instrument, start, end, limit=10):
+    for record in get_exposures_by_period(butler, instrument, start, end):
         if record.timespan.end < end:
             exposure_list.append(record)
             min_topic_time = min(record.timespan.begin, min_topic_time)
             max_topic_time = max(record.timespan.begin, max_topic_time)
 
-    for record in get_visits_by_period(butler, instrument, start, end, limit=10):
-        if record.timespan.end < end:
-            visit_list.append(record)
-            min_topic_time = min(record.timespan.begin, min_topic_time)
-            max_topic_time = max(record.timespan.begin, max_topic_time)
+    # for record in get_visits_by_period(butler, instrument, start, end, limit=10):
+    #     if record.timespan.end < end:
+    #         visit_list.append(record)
+    #         min_topic_time = min(record.timespan.begin, min_topic_time)
+    #         max_topic_time = max(record.timespan.begin, max_topic_time)
 
     print(f"Exposures: {len(exposure_list)}")
     print(f"Visits: {len(visit_list)}")
     print(f"Min Topic time: {min_topic_time}")
     print(f"Max Topic time: {max_topic_time}")
 
+    # Get data for all Topics and fields
+    datalake = {}
+    for topic in config["topics"]:
+        efd_series = await get_efd_values(efd, topic, min_topic_time, max_topic_time)
+        datalake[topic['name']] = efd_series
+    # print(datalake)
+
+    results = []
+    for exposure in exposure_list[35:45]:
+        new_row = {
+            "exposure_id": exposure.id, 
+            "instrument": exposure.instrument
+        }
+
+        # Proccess Columns
+        for column in config["columns"]:
+            summary = proccess_column(column, data=datalake, begin=exposure.timespan.begin, end=exposure.timespan.end)
+            new_row[column["name"]] = summary
+
+        results.append(new_row)
+
+    print("--------------------------")
+    for r in results:
+        print(r)
+
+    
     # exposure_records = Records(db)
     # print("Exposure Records:")
     # print(exposure_records)
@@ -178,9 +279,7 @@ async def process_interval(
     # exposure_records.write(config["exposure_table"])
     # visit_records.write(config["visit_table"])
 
-    for topic in config["topics"]:
-        efd_values = await get_efd_values(efd, topic, min_topic_time, max_topic_time)
-        print(efd_values)
+
 
 
 def build_argparser() -> argparse.ArgumentParser:
